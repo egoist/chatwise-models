d9befcc5d24b69c898e7273f382dbb9eb423abf3
EGOIST <hi@egoist.dev>
2025-08-01 18:38:21 +0800

    fix zhipu

--- data/bigmodel.ts
+++ data/bigmodel.ts
@@ -6,8 +6,8 @@ export default [
     name: "GLM-4.5",
     description:
       "高智能旗舰 - 性能最优，强大的推理能力、代码生成能力以及工具调用能力",
-    contextLimit: 128_000,
-    maxOutputLimit: 96_000,
+    contextLimit: 131_072,
+    maxOutputLimit: 98_304,
     price: {
       inputText: 0.8,
       outputText: 2.0,
@@ -19,8 +19,8 @@ export default [
     name: "GLM-4.5-Air",
     description:
       "高性价比 - 同参数规模性能最佳，在推理、编码和智能体任务上表现强劲",
-    contextLimit: 128_000,
-    maxOutputLimit: 96_000,
+    contextLimit: 131_072,
+    maxOutputLimit: 98_304,
     price: {
       inputText: 0.8,
       outputText: 2.0,
@@ -32,8 +32,8 @@ export default [
     name: "GLM-4.5-X",
     description:
       "高智能旗舰-极速版 - 推理速度更快，适用于搜索问答、智能助手、实时翻译等时效性较强场景",
-    contextLimit: 128_000,
-    maxOutputLimit: 96_000,
+    contextLimit: 131_072,
+    maxOutputLimit: 98_304,
     price: {
       inputText: 0.8,
       outputText: 2.0,
@@ -45,8 +45,8 @@ export default [
     name: "GLM-4.5-AirX",
     description:
       "高性价比-极速版 - 推理速度快，且价格适中，适用于时效性有较强要求的场景",
-    contextLimit: 128_000,
-    maxOutputLimit: 96_000,
+    contextLimit: 131_072,
+    maxOutputLimit: 98_304,
     price: {
       inputText: 0.8,
       outputText: 2.0,
@@ -57,16 +57,16 @@ export default [
     id: "glm-4.5-flash",
     name: "GLM-4.5-Flash",
     description: "免费模型 - 最新基座模型的普惠版本",
-    contextLimit: 128_000,
-    maxOutputLimit: 96_000,
+    contextLimit: 131_072,
+    maxOutputLimit: 98_304,
   },
   {
     id: "glm-4-plus",
     name: "GLM-4-Plus",
     description:
       "性能优秀 - 性能最优，语言理解、逻辑推理、指令遵循、长文本处理效果领先",
-    contextLimit: 128_000,
-    maxOutputLimit: 4_000,
+    contextLimit: 131_072,
+    maxOutputLimit: 4_096,
     price: {
       inputText: 5.0,
       outputText: 5.0,
@@ -77,8 +77,8 @@ export default [
     id: "glm-4-air-250414",
     name: "GLM-4-Air-250414",
     description: "高性价比 - 快速执行复杂任务，擅长工具调用、联网搜索、代码",
-    contextLimit: 128_000,
-    maxOutputLimit: 16_000,
+    contextLimit: 131_072,
+    maxOutputLimit: 16_384,
     price: {
       inputText: 5.0,
       outputText: 5.0,
@@ -89,8 +89,8 @@ export default [
     id: "glm-4-airx",
     name: "GLM-4-AirX",
     description: "极速推理 - 超快的推理速度，强大的推理效果",
-    contextLimit: 8_000,
-    maxOutputLimit: 4_000,
+    contextLimit: 8_192,
+    maxOutputLimit: 4_096,
     price: {
       inputText: 5.0,
       outputText: 5.0,
@@ -101,8 +101,8 @@ export default [
     id: "glm-4-flashx",
     name: "GLM-4-FlashX",
     description: "高速低价 - Flash 增强版本，超快推理速度，更快并发保障",
-    contextLimit: 128_000,
-    maxOutputLimit: 16_000,
+    contextLimit: 131_072,
+    maxOutputLimit: 16_384,
     price: {
       inputText: 5.0,
       outputText: 5.0,
@@ -113,8 +113,8 @@ export default [
     id: "glm-4-flashx-250414",
     name: "GLM-4-FlashX-250414",
     description: "高速低价 - Flash 增强版本，超快推理速度，更快并发保障",
-    contextLimit: 128_000,
-    maxOutputLimit: 16_000,
+    contextLimit: 131_072,
+    maxOutputLimit: 16_384,
     price: {
       inputText: 5.0,
       outputText: 5.0,
@@ -125,8 +125,8 @@ export default [
     id: "glm-z1-air",
     name: "GLM-Z1-Air",
     description: "高性价比 - 高性价比，具备深度思考能力，数理推理能力显著增强",
-    contextLimit: 128_000,
-    maxOutputLimit: 32_000,
+    contextLimit: 131_072,
+    maxOutputLimit: 32_768,
     price: {
       inputText: 0.5,
       outputText: 0.5,
@@ -138,8 +138,8 @@ export default [
     name: "GLM-Z1-AirX",
     description:
       "极速推理 - 国内最快的推理速度，支持 8 倍推理速度，问题即问即答",
-    contextLimit: 32_000,
-    maxOutputLimit: 30_000,
+    contextLimit: 32_768,
+    maxOutputLimit: 30_720,
     price: {
       inputText: 0.5,
       outputText: 0.5,
@@ -150,15 +150,15 @@ export default [
     id: "glm-z1-flash",
     name: "GLM-Z1-Flash",
     description: "免费模型 - 复杂任务推理，轻量级应用",
-    contextLimit: 128_000,
-    maxOutputLimit: 32_000,
+    contextLimit: 131_072,
+    maxOutputLimit: 32_768,
   },
   {
     id: "glm-z1-flashx",
     name: "GLM-Z1-FlashX",
     description: "高速低价 - 超快推理速度，更快并发保障，极致性价比",
-    contextLimit: 128_000,
-    maxOutputLimit: 32_000,
+    contextLimit: 131_072,
+    maxOutputLimit: 32_768,
     price: {
       inputText: 0.5,
       outputText: 0.5,
